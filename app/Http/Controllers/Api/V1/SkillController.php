d334e83e6051e3a30beba9aeaff30b6e1a703b25
Emanuelle <emanuelle.cortez@gamese.gg>
2024-04-14 19:10:27 -0300

    Created Delete method to Api

--- app/Http/Controllers/Api/V1/SkillController.php
+++ app/Http/Controllers/Api/V1/SkillController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreSkillRequest;
+use App\Http\Resources\V1\SkillResource;
+use App\Http\Resources\V1\SkillCollection;
 use App\Models\Skill;
 use Illuminate\Http\Request;
 
@@ -11,7 +13,12 @@ class SkillController extends Controller
 {
     public function index()
     {
-        return response()->json("Skill Index");
+        return new SkillCollection(Skill::all());
+    }
+
+    public function show(Skill $skill)
+    {
+        return new SkillResource($skill);
     }
 
     public function store(StoreSkillRequest $request)
@@ -24,5 +31,12 @@ class SkillController extends Controller
     {
         $skill->update($request->validated());
         return response()->json("Skill Updated");
+    }
+
+    public function destroy(Skill $skill)
+    {
+        $skill->delete();
+
+        return response()->json("Skill Deleted");
     }
 }
